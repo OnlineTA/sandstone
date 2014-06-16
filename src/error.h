427a3bab15a207db9c8837f5ec6e16b31674f530
Oleksandr Shturmov <oleks@oleks.info>
2014-06-17 00:26:48 +0200

    type-o

--- src/error.h
+++ src/error.h
@@ -3,7 +3,7 @@
 
 #include <errno.h>  // errno
 #include <libgen.h> // basename
-#include <stdio.h>  // fprintfs, stderr
+#include <stdio.h>  // fprintf, stderr
 #include <string.h> // strerror
 
 #define ERROR_REPORT(msg) (fprintf(stderr, "%s:%d: %s - %s.\n", \
