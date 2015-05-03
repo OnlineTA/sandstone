0ff89803d492ed1696c86b22f476a674fabfb510
Oleksandr Shturmov <oleks@oleks.info>
2015-05-03 19:46:50 +0200

    make / rprivate as well

--- src/mntns-exec.c
+++ src/mntns-exec.c
@@ -4,21 +4,45 @@
 
 // mntns-exec
 //
-// Execute a program contained in a mount namespace. Largely equivalent to
-// unshare -m, but no reliance on a preexisting shell.
+// Execute a program contained in a new mount namespace, with all mounts below
+// / marked as private. Private is default, but someone might've made something
+// shared. This means that new mounts are neither propagated from host to
+// sandbox, nor from sandbox to host.
+//
+// This is equivalent to the following shell command:
+// $ unshare -m mount --make-rprivate /
+//
+// See also Documentation/filesystems/sharedsubtree.txt.
 
 #define _GNU_SOURCE     // Necessary to get CLONE_NEWNS from <sched.h>.
 #include <sched.h>      // CLONE_NEWNS
+#include <sys/mount.h>  // mount, MS_REC, MS_PRIVATE
 
 #include "ctrl-flow.h"  // stone_break, stone_continue
+
+static inline void
+unshare_mntns()
+{
+  if (unshare(CLONE_NEWNS) == -1)
+  {
+    stone_break("unshare mntns");
+  }
+}
+
+static inline void
+mark_root_rprivate()
+{
+  if (mount("none", "/", NULL, MS_REC | MS_PRIVATE, NULL) == -1)
+  {
+    stone_break("mark root rprivate");
+  }
+}
 
 int
 main(int argc, char *argv[])
 {
-  if (unshare(CLONE_NEWNS) == -1)
-  {
-    stone_break("unshare");
-  }
+  unshare_mntns();
+  mark_root_rprivate();
 
   stone_continue("continue", argc, argv);
 }
